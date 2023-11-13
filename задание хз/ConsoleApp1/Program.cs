366a2210c1b443f03a1b1d988407532aed42fbeb
Влад Зубанов <144595141+ktoyatakoy23@users.noreply.github.com>
2023-11-13 22:16:02 +0700

    Add files via upload

---
+++ задание хз/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+public class Example
+{
+    public static void Main()
+    {
+      
+
+        for(int i = 0; ;  i++) 
+        {
+            DateTime localDate = DateTime.Now;
+            DateTime utcDate = DateTime.UtcNow;
+            String[] cultureNames = { "ru-RU" };
+
+            foreach (var cultureName in cultureNames)
+            {
+                var culture = new CultureInfo(cultureName);
+                Console.WriteLine("   Текущее время: {0}",
+                                  localDate.ToString(culture), localDate.Kind);
+               
+            }
+        }
+        
+    }
+}
