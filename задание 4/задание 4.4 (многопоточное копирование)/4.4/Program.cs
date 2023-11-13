cb75903f058ba4df68d7761cd7ed58d8fdea282d
Влад Зубанов <144595141+ktoyatakoy23@users.noreply.github.com>
2023-11-13 21:54:19 +0700

    Add files via upload

---
+++ задание 4/задание 4.4 (многопоточное копирование)/4.4/Program.cs
@@ -0,0 +1,64 @@
+﻿//без многопоточности
+void nonMulti()
+{
+    string src = "C:\\test";
+    string dest = "C:\\test2";
+
+    if (Directory.Exists(src))
+    {
+        foreach (string filepath in Directory.GetFiles(src))
+        {
+
+            if (File.Exists(filepath))
+            {
+                string filename = new FileInfo(filepath).Name;
+                File.Copy(src + "\\" + filename, dest + "\\" + filename);
+                Console.WriteLine("Перемещен файл с именем: " + filename);
+            }
+            else
+            {
+                Console.WriteLine("файл(ы) не существует...");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Такой папки не существует...");
+    }
+}
+
+//c многопоточностью
+void multi()
+{
+    string src = "C:\\test";
+    string dest = "C:\\test2";
+
+    if (Directory.Exists(src))
+    {
+        string[] files = Directory.GetFiles(src);
+        Parallel.ForEach(files, files =>
+        {
+            if (File.Exists(files))
+            {
+                {
+                    string filename = new FileInfo(files).Name;
+                    File.Copy(src + "\\" + filename, dest + "\\" + filename);
+                    Console.WriteLine("Перемещен файл с именем: " + filename);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("файл(ы) не существует...");
+            }
+        });
+    }
+    else
+    {
+        Console.WriteLine("Такой папки не существует...");
+    }
+}
+
+//multi();
+
+nonMulti();
